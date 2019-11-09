f6b2b70693e4707fb1493ba4660039f45bd80da9
Ralph <ralphsliger1@gmail.com>
2019-11-08 19:52:06 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,112 @@
+import pygame
+import random
+
+# Inicializa pygame
+pygame.init()
+
+# Crea la pantalla (ancho y largo)
+screen = pygame.display.set_mode((800, 600))
+
+# Background
+background = pygame.image.load('background.jpg')
+
+# Titulo e icono
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('space-ship.png')
+pygame.display.set_icon(icon)
+
+# Player (Spaceship imagen, coordenadas)
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 4
+enemyY_change = 40
+
+# Bullet
+# Ready - no aparece en pantalla
+# Fire - aparece en pantalla
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 40
+bullet_state = "ready"
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
+# Game loop - se asegura que el juego siempre este corriendo
+
+running = True
+while running:
+    # RGB - Red, Green, Blue
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Verifica tecla presionada (Derecha, izqquierda)
+        # Keydown verifica si una tecla esta siendo presionada
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+            if event.key == pygame.K_SPACE:
+                # obtiene la coordenada x actual de la navae
+                if bullet_state is "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    # Respetar rango en pantalla (No se salga de los limites)
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Movimiento enemigo
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX = 4
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX = -4
+        enemyY += enemyY_change
+
+    # Movimieto bala - cambia de 0 a 480 y le cambia el estado a ready para volver a ser disparada
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    pygame.display.update()
